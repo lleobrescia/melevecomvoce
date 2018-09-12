59be4b1d65b764fcf745e66c8911dfa00980a465
Leo Brescia <lleobrescia@gmail.com>
2018-09-12 16:23:05 -0300

    Removidos arquivos desnecessarios.

--- wp-content/themes/melevecomvoce/functions.php
+++ wp-content/themes/melevecomvoce/functions.php
@@ -76,10 +76,10 @@ function melevecomvoce_setup()
    * @link https://codex.wordpress.org/Theme_Logo
    */
   add_theme_support('custom-logo', array(
-    'height'      => 250,
-    'width'       => 250,
-    'flex-width'  => true,
-    'flex-height' =>  true,
+    'height' => 250,
+    'width' => 250,
+    'flex-width' => true,
+    'flex-height' => true,
   ));
 }
 endif;
@@ -101,24 +101,6 @@ function melevecomvoce_content_width()
 }
 add_action('after_setup_theme', 'melevecomvoce_content_width', 0);
 
-/**
- * Register widget area.
- *
- * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
- */
-function melevecomvoce_widgets_init()
-{
-  register_sidebar(array(
-    'name' => esc_html__('Sidebar', 'melevecomvoce'),
-    'id' => 'sidebar-1',
-    'description' => esc_html__('Add widgets here.', 'melevecomvoce'),
-    'before_widget' => '<section id="%1$s" class="widget %2$s">',
-    'after_widget' => '</section>',
-    'before_title' => '<h2 class="widget-title">',
-    'after_title' => '</h2>',
-  ));
-}
-add_action('widgets_init', 'melevecomvoce_widgets_init');
 
 /**
  * Enqueue scripts and styles.
